c44d4fc1277291b33e4ac805df5978b57b39bdd0
IT24102374 <IT24102374@my.sliit.lk>
2025-09-15 09:56:57 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24102374.R
@@ -0,0 +1,22 @@
+# Setting the directory
+setwd("C:\\Users\\User\\Desktop\\IT24102374")
+
+
+# Question 1 - Part i
+# X ~ Binomial(n = 50, p = 0.85)
+
+
+# Question 1 - Part ii
+1 - pbinom(46, 50, 0.85)
+
+
+# Question 2 - Part i
+# X = Number of customer calls received in an hour
+
+
+# Question 2 - Part ii
+# X ~ Poisson(lambda = 12)
+
+
+# Question 2 - Part iii
+dpois(15, 12)
